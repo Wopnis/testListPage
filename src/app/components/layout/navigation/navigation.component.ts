6ecf1ca454f387bd30d7c4947165c059507261c8
Wopnis <wopnis2016@outlook.com>
2021-12-23 13:25:05 +0300

    deleted files

--- src/app/components/layout/navigation/navigation.component.ts
+++ src/app/components/layout/navigation/navigation.component.ts
@@ -20,20 +20,11 @@ export class NavigationComponent implements OnInit {
 
   constructor() { }
 
-  tabs = [1, 2, 3, 4];
+  tabs = ['income', 'outcome', 'loan', 'investment'];
   counter = this.tabs.length + 1;
   active: any;
 
-  close(event: MouseEvent, toRemove: number) {
-    this.tabs = this.tabs.filter(id => id !== toRemove);
-    event.preventDefault();
-    event.stopImmediatePropagation();
-  }
 
-  add(event: MouseEvent) {
-    this.tabs.push(this.counter++);
-    event.preventDefault();
-  }
 
   ngOnInit(): void {
   }
